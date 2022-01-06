54916ebdfe51e4c474d083e3049d27c0b9cfd4d4
Manol Donev <manol.donev@gmail.com>
2022-01-06 18:57:04 +0200

    chore: setup eslint and prettier

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,6 +1,7 @@
-import { ReportHandler } from 'web-vitals';
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import type { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
@@ -12,4 +13,5 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   }
 };
 
-export default reportWebVitals;
+export { reportWebVitals };
+/* eslint-enable @typescript-eslint/no-floating-promises */
